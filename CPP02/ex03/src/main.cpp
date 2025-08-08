3c0b63e40e491b8e96a3e605921be68df376c447
IgorKulik-es <igorkulik28@proton.me>
2025-08-08 17:52:38 +0200

    added missing variables

--- CPP02/ex03/src/main.cpp
+++ CPP02/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 09:48:01 by ikulik            #+#    #+#             */
-/*   Updated: 2025/08/06 19:36:21 by ikulik           ###   ########.fr       */
+/*   Updated: 2025/08/08 17:16:57 by ikulik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 int main( void )
 {
-	Point	a ( 0.0f, 0.0f );
-	Point	b ( 0.0f, 5.0f );
-	Point	c ( 5.0f, 0.0f );
-	Point	p ( -2.0f, 2.0f );
+	bool	answer;
 
-	bsp(a, b, c, p);
+	Point	a ( 1.0f, 1.0f );
+	Point	b ( 1.0f, 6.0f );
+	Point	c ( 6.0f, 1.0f );
+	Point	p ( 2.0f, -2.0f );
+
+	answer= bsp(a, b, c, p);
+	if (answer)
+		std::cout << "true" << std::endl;
+	else
+		std::cout
 
 	return 0;
 }
