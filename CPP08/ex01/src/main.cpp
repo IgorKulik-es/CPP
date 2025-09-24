acd2fa2371905a3179b3a7d66200655d0f090c75
IgorKulik-es <igorkulik28@proton.me>
2025-09-24 16:52:08 +0200

    updated the tester

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 14:57:22 by ikulik            #+#    #+#             */
-/*   Updated: 2025/09/24 14:50:18 by ikulik           ###   ########.fr       */
+/*   Updated: 2025/09/24 16:47:25 by ikulik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,67 @@
 int main()
 {
 	Span sp = Span(5);
+	Span small(3);
 
 	sp.addNumber(6);
 	sp.addNumber(3);
-	sp.addNumber(17);
 	sp.addNumber(9);
+	std::cout << "Assigning to another array of sufficient size:" << std::endl;
+	try
+	{
+		small = sp;
+		std::cout << "Array copied successfully" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	sp.addNumber(17);
 	sp.addNumber(11);
 
-	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << "Assigning an array of in sufficient size:" << std::endl;
+	try
+	{
+		small = sp;
+		std::cout << "Array copied successfully" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "Shortest and longest span of sp: ";
+	std::cout << sp.shortestSpan() << " ";
 	std::cout << sp.longestSpan() << std::endl;
+	std::cout << "Shortest and longest span of small: ";
+	std::cout << small.shortestSpan() << " ";
+	std::cout << small.longestSpan() << std::endl;
+
+	Span	big(10);
+
+	std::cout << "Adding multiple numbers to new Span." << std::endl;
+	try
+	{
+		big.addSpan(small.getContent().begin(), small.getContent().end());
+		big.addSpan(sp.getContent().begin(), sp.getContent().end());
+		std::cout << "Array copied successfully" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "New array content: ";
+	for (size_t i = 0; i < big.getContent().size(); i++)
+		std::cout << big.getContent()[i] << " ";
+	std::cout << "\nTrying to add over maximum:" << std::endl;
+	try
+	{
+		big.addSpan(sp.getContent().begin(), sp.getContent().end());
+		std::cout << "Array copied successfully" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
