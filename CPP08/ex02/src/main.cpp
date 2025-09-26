dc532ee7efc9cbc36336a5ca9051ae385c3cffc4
IgorKulik-es <igorkulik28@proton.me>
2025-09-26 19:40:22 +0200

    added tests

--- CPP08/ex02/src/main.cpp
+++ CPP08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 14:57:22 by ikulik            #+#    #+#             */
-/*   Updated: 2025/09/25 16:02:56 by ikulik           ###   ########.fr       */
+/*   Updated: 2025/09/26 16:50:49 by ikulik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int main()
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::iterator	it = mstack.begin();
+	MutantStack<int>::iterator	ite = mstack.end();
 	++it;
 	--it;
 	while (it != ite)
@@ -35,8 +35,14 @@ int main()
 		++it;
 	}
 
+	std::cout << "Testing copy constructor:" << std::endl;
 	MutantStack<int>	copy_stack(mstack);
-
+	while (copy_stack.size())
+	{
+		std::cout << copy_stack.top() << " ";
+		copy_stack.pop();
+	}
+	std::cout << std::endl;
 	std::stack<int> s(mstack);
 	return 0;
 }
