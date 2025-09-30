c0ec31eaf4bea120e72a54e4fb7b4fe9b89ba67d
IgorKulik-es <igorkulik28@proton.me>
2025-09-30 20:00:10 +0200

    start

---
+++ CPP09/ex02/include/Pmergeme.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Pmergeme.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 13:12:57 by ikulik            #+#    #+#             */
+/*   Updated: 2025/09/30 19:26:34 by ikulik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+# include <list>
+# include <deque>
+# include <iostream>
+
+template <typename T, typename T::iterator>
+void	pmergesort( typename T::iterator start, typename T::iterator end );
+
+template <typename Container>
+class Pmergeme
+{
+	public:
+
+}
+
+#endif
---
+++ CPP09/ex02/src/Pmergeme.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Pmergeme.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 14:24:09 by ikulik            #+#    #+#             */
+/*   Updated: 2025/09/30 19:21:35 by ikulik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Pmergeme.hpp"
+
+template <typename T, typename T::iterator>
+void
+
+
+
+
+
+
+template <typename T, typename T::iterator>
+void	pmergesort( typename T::iterator start, typename T::iterator end )
+{
+
+}
